9c499964f5978af1cbad729d420c9ca7269e5aff
zachariah stroup <zachariah2112@hotmail.com>
2023-01-23 19:26:46 -0700

    second commit

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,6 +1,10 @@
 const { ethers } = require("hardhat")
 
 const networkConfig = {
+    default: {
+        name: "hardhat",
+        keepersUpdateInterval: "30",
+    },
     5: {
         name: "goerli",
         vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
@@ -12,7 +16,7 @@ const networkConfig = {
     },
     31337: {
         // no vrfcoord because using mock and just copied gas lane from goerli for same reason
-        name: "hardhat",
+        name: "localhost",
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
         callbackGasLimit: "500000",
